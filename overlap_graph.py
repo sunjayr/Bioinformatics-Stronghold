79c0e7687e196e1b18198d4900f304832ebe6360
Sunjay Ravishankar <Sunjay@MacBook-Pro-168.attlocal.net>
2018-10-10 21:54:56 -0700

    Another commit

---
+++ overlap_graph.py
@@ -0,0 +1,44 @@
+from types import *
+fasta_sequences = []
+fasta_identifiers = []
+adjacency_list = []
+fasta_dict = {}
+
+def read_fasta_sequence(fasta_file):
+    global fasta_dict
+
+    with open(fasta_file, 'r') as f:
+        lines = f.read().split('\n')
+        sequence_buffer = ""
+        fasta_id = ""
+        for line in lines:
+            if line[0] == '>':
+                if sequence_buffer:
+                    fasta_sequences.append(sequence_buffer)
+                    fasta_identifiers.append(fasta_id)
+                    sequence_buffer = ""
+                fasta_id = line[1:]
+            else:
+                sequence_buffer += line
+    fasta_dict = dict(zip(fasta_sequences, fasta_identifiers))
+
+
+def create_adjacency_list(k):
+    assert type(k) is IntType, "K value is not an integer"
+    for i in range(0,len(fasta_sequences)):
+        for j in range(0,len(fasta_sequences)):
+            if fasta_sequences[i] == fasta_sequences[j]:
+                continue
+            if fasta_sequences[i][-k:] == fasta_sequences[j][:k]:
+                adjacency_list.append((fasta_dict[fasta_sequences[i]], fasta_dict[fasta_sequences[j]]))
+    
+    for tup in adjacency_list:
+        print tup[0] + ' ' + tup[1]
+
+def main():
+    read_fasta_sequence('overlap_input.txt')
+    create_adjacency_list(3)
+
+if __name__ == '__main__':
+    main()
+    
