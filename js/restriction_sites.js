42af9b54b50b1e77cee6d1cdfbd93cc779cebc7e
sunjayr <sunjay.ravishankar@gmail.com>
2019-08-24 12:08:19 -0700

    [feature/restriction-sites] implement generate kmers

--- js/restriction_sites.js
+++ js/restriction_sites.js
@@ -1,22 +1,30 @@
 const fs = require('fs');
 
 function readFasta() {
-    let fastaObj = {};
-    fs.readFile('./input/restriction_sites_input.txt', (err, data) => {
-        for (let line of data.toString().split('\n')) {
-            if (line.startsWith('>')) {
-
-            }
-        }
-    });
+    let data = fs.readFileSync('./input/restriction_sites_input.txt');
+    let sequence = data.toString().split('\n').filter(x => !x.startsWith('>'));
+    return sequence.join();
+    
 }
 
-function generateKmers(kmerLength) {
-    kmerList = [];
+function generateKmers(kmerLength, sequence) {
+    let kmerList = [];
+    for (let i = 0; i < sequence.length - kmerLength + 1; i++) {
+        kmerList.push(sequence.substring(i,i+kmerLength));
+    }
+    return kmerList;
+    
 }
 
 function main(){
-    readFasta();
+    let fastaSeq = readFasta();
+    let allKmers = [];
+    for (let i = 4; i <= 12; i++) 
+    {
+        allKmers.push(...generateKmers(i, fastaSeq));        
+    }
+    console.log(allKmers);
+    
 }
 
 main();
