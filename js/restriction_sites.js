8764662db4fb07ed0148fc4f96202fb730379999
sunjayr <sunjay.ravishankar@gmail.com>
2019-08-28 00:23:23 -0700

    [feature/restriction_sites] Implementation complete and testing

--- js/restriction_sites.js
+++ js/restriction_sites.js
@@ -8,22 +8,68 @@ function readFasta() {
 }
 
 function generateKmers(kmerLength, sequence) {
-    let kmerList = [];
+    let kmerMap = {};
     for (let i = 0; i < sequence.length - kmerLength + 1; i++) {
-        kmerList.push(sequence.substring(i,i+kmerLength));
+        let k = sequence.substring(i,i+kmerLength);
+        if (kmerMap.hasOwnProperty(k)) {
+            kmerMap[k].push(i + 1);
+        }
+        else {
+            kmerMap[k] = [i + 1];        
+        }
     }
-    return kmerList;
+    return kmerMap;
     
+}
+
+function isRestrictionSite(kmer) {
+    let translate_map = {
+        'A':'T',
+        'C':'G',
+        'T':'A',
+        'G':'C'
+    };
+    let sequence = [];
+    for (let base of kmer) {
+        sequence.push(translate_map[base]);
+    }
+    let complement = sequence.reverse().join('')
+    return kmer === complement ? true : false;
+}
+
+function locateRestrictionSites(kmers) {
+    for (let k in kmers) {
+        if(!isRestrictionSite(k)){
+            delete kmers[k];
+        }    
+    }
+    return kmers;
+}
+
+function outputPositions(kmerMap) {
+    let position_list = [];
+    for (let k in kmerMap) {
+        for (let pos of kmerMap[k]) {
+            position_list.push([pos, k.length]);
+        }
+    }
+    let sorted_list = position_list.sort((x,y) => {
+        return x[0] >= y[0];
+    });
+    for (let p of sorted_list) {
+        console.log(`${p[0]}\t${p[1]}`)
+    }
 }
 
 function main(){
     let fastaSeq = readFasta();
-    let allKmers = [];
+    let allKmers = {};
     for (let i = 4; i <= 12; i++) 
     {
-        allKmers.push(...generateKmers(i, fastaSeq));        
+        allKmers = Object.assign(allKmers, generateKmers(i, fastaSeq));        
     }
-    console.log(allKmers);
+    let filteredKmers = locateRestrictionSites(allKmers);
+    outputPositions(filteredKmers);
     
 }
 
