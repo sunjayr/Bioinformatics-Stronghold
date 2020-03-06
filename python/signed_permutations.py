d8a0879363b24740457d96f5e5e4e8375a0e8257
sunjayr <sunjay.ravishankar@gmail.com>
2020-03-05 22:53:36 -0800

    fix signed perms

--- python/signed_permutations.py
+++ python/signed_permutations.py
@@ -1,22 +1,13 @@
 import itertools
 import copy
-
-def permutation_length(n):
-    permutation = [i for i in range(1,n+1)]
-    all_permutations.extend(list(itertools.permutations(permutation)))
-    fixed_index = 0
-    while fixed_index < len(permutation):
-        perm_copy = copy.deepcopy(permutation)
-        perm_copy = map(lambda x: x * -1, perm_copy[:fixed_index]) + perm_copy[fixed_index:]
-        for i in range(fixed_index, len(permutation)):
-            perm_copy[i] = perm_copy[i] * -1
-            all_permutations.extend(list(itertools.permutations(perm_copy)))
-            perm_copy[i] = perm_copy[i] * -1
-        fixed_index += 1
                 
 if __name__ == "__main__":
     all_permutations = []
-    permutation_length(3)    
+    n = 5
+    int_pairs = [[i, -1 * i] for i in range(1, n + 1)]
+    signed_combos = map(list,list(itertools.product(*int_pairs)))
+    for p in signed_combos:
+        all_permutations += list(itertools.permutations(p))
     print len(all_permutations)
-    for perm in all_permutations:
-        print ' '.join(map(str, list(perm)))
+    for l in all_permutations:
+        print ' '.join(map(str, l))
